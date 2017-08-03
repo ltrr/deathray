3227e844ed6cc925379238b368a15952d560fc89
Lucas Torres <lucas.trr0@gmail.com>
2017-08-03 07:37:37 -0300

    Change ppm mode to P6

--- image.cpp
+++ image.cpp
@@ -1,14 +1,13 @@
 #include "image.hpp"
 
 void Image::write_ppm(std::ostream& out) {
-    out << "P3\n";
+    out << "P6\n";
     out << w << " " << h << "\n";
     out << "255\n";
     for (int i = 0; i != h; i++) {
         for (int j = 0; j != w; j++) {
             Color4u c = (*this)(i, j);
-            out << int(c.r) << " " << int(c.g) << " " << int(c.b) << " ";
+            out << c.r << c.g << c.b;
         }
-        out << "\n";
     }
 }
