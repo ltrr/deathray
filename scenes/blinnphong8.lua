70ed25e54cd67b466b05ad5d5f2696b3fdb79d1d
Lucas Torres <lucas.trr0@gmail.com>
2017-09-07 19:47:26 -0300

    Add blinn-phong test scenes

---
+++ scenes/blinnphong8.lua
@@ -0,0 +1,56 @@
+name = "images/blinnphong8.ppm"
+type = "ppm"
+codification = "binary"
+width = 1200
+height = 600
+
+samples = 1
+
+viewport = mkviewport {
+    width = width,
+    height = height
+}
+
+camera = lookat {
+    origin = { 0, 0, 0 },
+    target = { 0, 0, -1 },
+    up = { 0, 1, 0 },
+    fov = 2 * math.atan(2),
+    aspect = width / height
+}
+
+mat = lambert { 0.5, 0.5, 0.5 }
+
+scene = mkscene {
+    sphere {
+        center = { 0, -100.5, -1 },
+        radius = 100,
+        material = blinn_phong {
+            ambient = {0.1, 0.1, 0.1},
+            diffuse = {0.4, 0.4, 0.4},
+            specular = {1, 1, 1},
+            shiness = 5
+        }
+    },
+    sphere {
+        center = { 0, 0, -1 },
+        radius = 0.4,
+        material = blinn_phong {
+            ambient = {0.1, 0.1, 0.1},
+            diffuse = {0.0, 0.3, 0.8},
+            specular = {0.9, 0.9, 0.9},
+            shiness = 64
+        }
+    },
+    sun {
+        direction = { 20, 10, 5 },
+        intensity = { 1, 1, 1 }
+    },
+    bg = sky {
+        zenith = { 0.5, 0.7, 1 },
+        nadir = { 1, 1, 1 }
+    },
+    ambient = { 0.4, 0.4, 0.4 }
+}
+
+shader = blinn_phong_shader()
