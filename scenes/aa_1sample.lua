dc1e6ee529c16e8f15d027a29d84978c592ea8b8
Lucas Torres <lucas.trr0@gmail.com>
2017-09-07 18:49:02 -0300

    Add anti-aliasing test scenes

---
+++ scenes/aa_1sample.lua
@@ -0,0 +1,39 @@
+name = "images/aa_1sample.ppm"
+type = "ppm"
+codification = "binary"
+width = 1200
+height = 600
+
+samples = 1
+
+viewport = mkviewport {
+    width = width,
+    height = height
+}
+
+camera = lookat {
+    origin = { 0, 0, 0 },
+    target = { 0, 0, -1 },
+    up = { 0, 1, 0 },
+    fov = 2 * math.atan(2),
+    aspect = width / height
+}
+
+mat = lambert { 0.5, 0.5, 0.5 }
+
+scene = mkscene {
+    sphere {
+        center = { 0, -100.5, -3 },
+        radius = 100,
+        material = mat
+    },
+    sphere {
+        center = { 0, 0, -1 },
+        radius = 0.5,
+        material = mat
+    },
+    bg = sky {
+        zenith = { 1, 1, 1 },
+        nadir = { 1, 1, 1 }
+    }
+}
---
+++ scenes/aa_8samples.lua
@@ -0,0 +1,39 @@
+name = "images/aa_8samples.ppm"
+type = "ppm"
+codification = "binary"
+width = 1200
+height = 600
+
+samples = 8
+
+viewport = mkviewport {
+    width = width,
+    height = height
+}
+
+camera = lookat {
+    origin = { 0, 0, 0 },
+    target = { 0, 0, -1 },
+    up = { 0, 1, 0 },
+    fov = 2 * math.atan(2),
+    aspect = width / height
+}
+
+mat = lambert { 0.5, 0.5, 0.5 }
+
+scene = mkscene {
+    sphere {
+        center = { 0, -100.5, -1 },
+        radius = 100,
+        material = mat
+    },
+    sphere {
+        center = { 0, 0, -1 },
+        radius = 0.5,
+        material = mat
+    },
+    bg = sky {
+        zenith = { 0.5, 0.7, 1 },
+        nadir = { 1, 1, 1 }
+    }
+}
