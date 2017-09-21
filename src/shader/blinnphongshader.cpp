f4e1f325f7af64055e0402fb7f0a05ba353ef70c
Lucas Torres <lucas.trr0@gmail.com>
2017-09-21 12:00:08 -0300

    Add shadow to Blinn-Phong shader

--- src/shader/blinnphongshader.cpp
+++ src/shader/blinnphongshader.cpp
@@ -3,15 +3,19 @@
 
 Vec3 BlinnPhongShader::colorAt(const ScenePtr& scene, const Ray& ray)
 {
-    Hit hit;
+    Hit hit, shit;
     if (scene->hit(ray, 0, std::numeric_limits<float>::max(), hit)) {
         Color3f color(hit.material->ambient() * scene->ambient());
         for (auto light : scene->lights()) {
             Vec3 pointToLight = light->directionFrom(hit.point);
 
-            float cos_nl = max(0.0f, dot(pointToLight, hit.normal));
-            //if (cos_nl <= 0) continue;
+            if (scene->hit(
+                Ray(hit.point, pointToLight),
+                1000*EPS, std::numeric_limits<float>::max(), shit)) {
+                continue;
+            }
 
+            float cos_nl = max(0.0f, dot(pointToLight, hit.normal));
             Vec3 half = unit(pointToLight - ray.dir());
 
             color += light->radianceAt(hit.point) * (
